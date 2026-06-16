0aa5215dfdef084ef3267ed833d057bdd968d981
Juan1940 <scze.juanignacio.suarez.vi@unifranz.edu.bo>
2026-06-15 22:34:07 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host     = "://aivencloud.com"; // El "Host" de Aiven
-$port     = "12345";                       // El "Port" de Aiven
+$host     = "mysql-1605f630-formwebunifranz.a.aivencloud.com"; // El "Host" de Aiven
+$port     = "23092";                       // El "Port" de Aiven
 $user     = "avnadmin";                    // El "User" de Aiven
-$password = "tu_contraseña_de_aiven";      // La contraseña de Aiven
+$password = "AVNS_6LKSG3dKJPUM0vwU_ap";      // La contraseña de Aiven
 $dbname   = "defaultdb";                   // Tu base de datos
 
 $conexion = mysqli_connect($host, $user, $password, $dbname, $port);
